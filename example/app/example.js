3fd712e7bbd81f1bad3130d282225f63b2a7d7ba
Charles Liao <charleslxh@icloud.com>
2017-01-20 14:37:06 +0800

    add a chat demo in example

--- example/app/example.js
+++ example/app/example.js
@@ -1,29 +1,104 @@
 'use strict'
 
-var Counter = React.createClass({
+var Chat = React.createClass({
   getInitialState: function () {
-    return { messages: [] };
+    return {
+      user: null,
+      users: [],
+      currentUser: {
+        messages: []
+      }
+    };
   },
   contextTypes: {
     socket: React.PropTypes.object.isRequired
   },
   componentDidMount: function() {
     this.refs.chatTextarea.focus();
+    this.context.socket.emit('user:login');
   },
   scrollToBottom: function(name) {
     if (this.refs[name]) {
       this.refs[name].scrollTop = this.refs.messages.scrollHeight;
     }
   },
-  onMessage: function (message) {
-    console.debug('Got event message_new: ', message);
-    var messages = this.state.messages;
-    messages.push(message);
-    this.setState({messages: messages});
-    this.scrollToBottom('messages');
+  selectUser: function(index){
+    var users = this.state.users;
+    var selectedUser= this.state.users[index];
+    selectedUser.unreadMessageCount = 0;
+    users[index] = selectedUser;
+    this.setState({users: users});
+    this.setState({currentUser: this.state.users[index]});
+  },
+  loadOnlineUsers: function() {
+    this.context.socket.emit('users:get');
+  },
+  handleMessageNew: function (message) {
+    console.info('Got event message_new: ', message);
+    var currentUser = this.state.currentUser;
+    var users = this.state.users;
+    var index = users.findIndex(function(u) { return message.from === u.userId });
+    var messageFromUser = users[index];
+
+    if (!messageFromUser) {
+      console.warn('Not found user!')
+      return;
+    }
+
+    messageFromUser.messages.push(message);
+
+    if (currentUser && currentUser.nickname === messageFromUser.nickname ) {
+      this.scrollToBottom('messages');
+    } else {
+      if (messageFromUser.unreadMessageCount) {
+        messageFromUser.unreadMessageCount += 1;
+      } else {
+        messageFromUser.unreadMessageCount = 1
+      }
+    }
+    console.log(messageFromUser)
+    users.splice(index, 1);
+    users.unshift(messageFromUser);
+    this.setState({users: users});
+  },
+  handleEventResponse: function(response) {
+    console.debug('Got response event: ', response.type, response.data);
+    switch (response.type) {
+      case 'user:login':
+        this.setState({user: response.data})
+        this.loadOnlineUsers();
+        break;
+      case 'users:get':
+        var users = Array.isArray(response.data) ? response.data : [response.data]
+        this.setState({users: users})
+        break;
+      case 'message:send':
+        break;
+      case 'user:logout':
+        break;
+    }
+  },
+  handleUserAdded: function(user) {
+    console.info('Got user:added event: ', user);
+    var users = this.state.users;
+    user.messages = []
+    users.push(user);
+    this.setState({users: users});
+  },
+  handleUserRemoved: function(user) {
+    console.info('Got user:removed event: ', user);
+    var users = this.state.users;
+    var index = users.findIndex(function(u) { user.userId === u.userId; });
+    users.splice(index, 1);
+    this.setState({users: users});
   },
   sendMessage: function() {
     var msg = this.refs.chatTextarea.value;
+
+    if (!this.state.user) {
+      console.warn('Not logged in!');
+      return;
+    }
 
     if (!this.context.socket || this.context.socket.status !== 'connected') {
       console.warn('Socket is unavailable!');
@@ -35,18 +110,20 @@ var Counter = React.createClass({
       return;
     }
 
+    var currentUser = this.state.currentUser;
+
     var message = {
       content: this.refs.chatTextarea.value,
-      from: 'Client',
-      time: new Date().getTime()
+      from: this.state.user.userId,
+      time: new Date().getTime(),
+      to: currentUser.userId
     };
 
-    this.context.socket.emit('message_send', message);
+    this.context.socket.emit('message:send', message);
 
     message.isMine = true;
-    var messages = this.state.messages;
-    messages.push(message);
-    this.setState({messages: messages}, function() {
+    currentUser.messages.push(message);
+    this.setState({currentUser: currentUser}, function() {
       this.scrollToBottom('messages');
     });
     this.refs.chatTextarea.value = ''
@@ -74,40 +151,73 @@ var Counter = React.createClass({
   },
   render: function () {
     var self = this;
+
     return (
       <div>
         <h1>React SocketIO Demo</h1>
-        <ReactSocketIO.Event event="message_new" handler={this.onMessage} />
-        <div className="chat" ref="chat">
-          <div className="tip">Come on, chat with server... ...</div>
-          <div className="messages" ref="messages">
-            {
-              self.state.messages.map(function (message, index) {
-                var classes = "message";
-                if (message.isMine) classes += " isMine"
-                var time = new Date(message.time);
-                return (
-                  <div className={classes} key={index}>
-                    <div className="title">
-                      <span className="name">【{message.from}】</span>
-                      <span className="time">{time.toLocaleTimeString()}</span>
+        <ReactSocketIO.Event event="message:new" handler={self.handleMessageNew} />
+        <ReactSocketIO.Event event="response" handler={self.handleEventResponse} />
+        <ReactSocketIO.Event event="user:added" handler={self.handleUserAdded} />
+        <ReactSocketIO.Event event="user:removed" handler={self.handleUserRemoved} />
+        <div className="chat-section" ref="chat">
+          <div className="chat-users-section">
+            <div className="title">Online Users</div>
+            <div className="users">
+              {
+                self.state.users.map(function (user, index) {
+                  var unreadMessageCountClass = "unreadMessageCount";
+                  var userClass = "user";
+
+                  if (user.unreadMessageCount <= 0) unreadMessageCountClass += " hidden"
+                  if (self.state.currentUser && user.nickname === self.state.currentUser.nickname) {
+                    userClass += " active";
+                  }
+
+                  return (
+                    <div className={userClass} key={index} onClick={function(e){self.selectUser(index)}}>
+                      <img className="avatar" src="https://dn-daniujia.qbox.me/moren.png" />
+                      <div className="others">
+                        <span className="username">{user.username}</span>
+                      </div>
+                      <div className={unreadMessageCountClass}>{user.unreadMessageCount}</div>
                     </div>
-                    <pre className="content">
-                      {message.content}
-                    </pre>
-                  </div>
-                )
-              })
-            }
+                  )
+                })
+              }
+            </div>
           </div>
-          <div className="footer">
-            <textarea
-              className="chat-textarea"
-              placeholder='Input there, press "Enter" to send message or "Ctrl + Enter" to add a newline.'
-              ref="chatTextarea"
-              onKeyDown={ this.handleKeyEvent }
-            >
-            </textarea>
+          <div className="chat-body-section">
+            <div className="title">{self.state.currentUser && self.state.currentUser.username}</div>
+            <div className="messages" ref="messages">
+              {
+                self.state.currentUser.messages.map(function (message, index) {
+                  var classes = "message";
+                  if (self.state.user && self.state.user.userId === message.from) classes += " isMine";
+
+                  var time = new Date(message.time);
+                  return (
+                    <div className={classes} key={index}>
+                      <div className="info">
+                        <span className="name"></span>
+                        <span className="time">{time.toLocaleTimeString()}</span>
+                      </div>
+                      <pre className="content">
+                        {message.content}
+                      </pre>
+                    </div>
+                  );
+                })
+              }
+            </div>
+            <div className="footer">
+              <textarea
+                className="chat-textarea"
+                placeholder='Input there, press "Enter" to send message or "Ctrl + Enter" to add a newline.'
+                ref="chatTextarea"
+                onKeyDown={ self.handleKeyEvent }
+              >
+              </textarea>
+            </div>
           </div>
         </div>
       </div>
@@ -122,7 +232,7 @@ var AppContainer = React.createClass({
 
     return (
       <ReactSocketIO.Socket uri={uri} options={options}>
-        <Counter />
+        <Chat />
       </ReactSocketIO.Socket>
     )
   }
--- example/server.js
+++ example/server.js
@@ -4,6 +4,9 @@ var fs = require('fs');
 
 app.listen(8090);
 
+var sockets = {}
+var userCounts = 0
+
 function handler (req, res) {
   var filePath = req.url;
 
@@ -21,18 +24,73 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
-  socket.on('message_send', function (message) {
-    console.log('message_send: ', message);
+  socket.on('user:login', function(data) {
+    var userId = ++userCounts;
+    socket.user = {
+      userId: userId,
+      username: 'User ' + userId,
+      nickname: 'user' + userId
+    };
+    console.log('response_user:login: ', socket.user);
+    socket.emit('response', { type: 'user:login', data: socket.user });
+    Object.keys(sockets).map(function(userId) {
+      sockets[userId].emit('user:added', socket.user)
+    });
 
-    setTimeout(function() {
-      var newMessage = {
-        from: 'Server',
-        time: new Date().getTime(),
-        content: 'I got you message, you said: ' + message.content
-      };
+    sockets[userId] = socket
+  });
 
-      socket.emit('message_new', newMessage);
-    }, 1000);
+  socket.on('users:get', function(data) {
+    var data = Object.keys(sockets).map(function(userId) {
+      var user = sockets[userId].user
+      return {
+        userId: user.userId,
+        username: user.username,
+        nickname: user.nickname,
+        unreadMessageCount: 0,
+        messages: []
+      }
+    }).filter(function(user) {
+      if (user.userId === socket.user.userId) return false;
+
+      if (data && data.target && data.targetId !== user.userId) return false;
+
+      return true;
+    });
+    console.log('response_users:get: ', data);
+    socket.emit('response', { type: 'users:get', data: data });
+  });
+
+  socket.on('message:send', function (message) {
+    message.time = new Date().getTime();
+    console.log('response_message:send: ', message);
+    sockets[message.to] && sockets[message.to].emit('message:new', message);
+    socket.emit('response', { type: 'message:send', data: message });
+  });
+
+  socket.on('user:logout', function() {
+    var user = socket.user;
+    socket.emit('response', { type: 'user:logout'});
+
+    if (!user) return;
+    delete sockets[user.userId];
+    socket.disconnect(true);
+
+    Object.keys(sockets).map(function(userId) {
+      sockets[userId].emit('user:logged:out', user)
+    });
+  });
+
+  socket.on('disconnect', function() {
+    var user = socket.user;
+    if (!user) return;
+
+    delete sockets[user.userId];
+    socket.disconnect(true);
+
+    Object.keys(sockets).map(function(userId) {
+      sockets[userId].emit('user:removed', user);
+    });
   });
 });
 
